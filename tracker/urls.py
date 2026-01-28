eb61b8facd93d13113a8f1674c7560d9f10cfd0b
Upasana Mukherjee <upashana2102@gmail.com>
2026-01-28 17:52:51 +0530

    Added remaining project files

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('delete-transaction/<id>/', views.delete_transaction, name="delete_transaction")
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import TrackingHistory, CurrentBalance
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+        current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+        expense_type = "CREDIT"
+        if float(amount) < 0:
+            expense_type = "DEBIT"
+
+        if float(amount) == 0:
+            messages.success(request, "Amount cannot be zero") 
+
+        tracking_history = TrackingHistory.objects.create(
+            amount = amount,
+            expense_type = expense_type,
+            current_balance = current_balance,
+            description = description,
+        )
+        current_balance.current_balance += float(tracking_history.amount)
+        current_balance.save()
+        return redirect('/')
+    
+    current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+    if not TrackingHistory.objects.exists():
+        current_balance.current_balance = 0
+        current_balance.save()
+    income = 0
+    expense = 0
+
+    for tracking_history in TrackingHistory.objects.all():
+        if tracking_history.expense_type=="CREDIT":
+            income += tracking_history.amount
+        else:
+            expense += tracking_history.amount
+    context = {'income': income, 'expense': expense,'transactions' : TrackingHistory.objects.all(), 'current_balance': current_balance}
+    return render(request, "index.html", context)
+
+def delete_transaction(request, id):
+    tracking_history = TrackingHistory.objects.filter(id=id)
+     
+    if tracking_history.exists():
+        current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+        tracking_history = tracking_history[0]
+        current_balance.current_balance = current_balance.current_balance - tracking_history.amount
+        current_balance.save()
+
+
+
+
+    tracking_history.delete()
+    return redirect('/')
